a8caade1e43b57678fb7641de70db2cf7f78aac1
Nevskiy <dmitriy.a.nevskiy@gmail.com>
2016-05-29 22:50:26 +0300

    1

---
+++ footer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @package antonia
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer class="site-footer" role="contentinfo">
+		<div class="site-info">
+			Site designed by <a href="http://nevskiy.com" rel="designer">Nevskiy</a>
+		</div><!-- .site-info -->
+	</footer>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
