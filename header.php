76d237c53b62766c52728539b83a1bd308111c93
Nevskiy <dmitriy.a.nevskiy@gmail.com>
2016-06-02 18:39:39 +0300

    plugin SEO

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="description">
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <!-- http://realfavicongenerator.net -->
 <?php wp_head(); ?>
